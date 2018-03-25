c64b0c62506cb134fc75dc36e73ccf59d3e8b19a
Costin Smilovici <costinsf11@gmail.com>
2018-03-25 19:30:46 +0000

    updated look and function

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ $(document).ready(function(){
     //check if user is right  and return booleans
     function checkSolution(userInput, correctAnswer){
         if(userInput===correctAnswer) {
-            return(true)
+            return(true);
         } else {
-            return(false)
+            return(false);
     }}
     
     //update htmll page whenevr player gets question correct
@@ -20,21 +20,47 @@ $(document).ready(function(){
         $(".col_2").html(userInput);
     }
     //updates robot when player gets html correct
-    function updateRobot(){
-        
+    function updateInstructions(currentLevel){
+        $("#text").text(levels[currentLevel].instructions);
+        $("").attr("src",levels);
     }
     //updates page when player is incorrect
     function showError(){
-        $(".col_2").html("<h1>You have an error!</h1>");
+        $(".col_2").html("<div class=\"alert alert-danger\" role=\"alert\">Your answer is incorrect! Try Again.</div>");
     }
-
+    function clearContent(){
+        $(".userInput").val('');
+        $(".col_2").empty();
+    }
+    var currentLevel = 0;
+    $("#next").hide();
     $("#submit").click(function(){
         var userInput = getInput();
-        if(checkSolution(userInput, "anything") === true){
-            console.log("correct");
+        if(checkSolution(userInput, levels[currentLevel].correctAnswer)){
+            updateHTML(userInput);
+            currentLevel = currentLevel + 1;
+            $("#next").show();
         } else {
-            console.log("false");
+            showError();
         }
     });
-    
+    $("#next").click(function(){
+        updateInstructions(currentLevel);
+        clearContent();
+        $("#next").hide();
+    });
+     var levels=[
+    {
+        difficulty:"very easy",
+        instructions:'Make header with "I Robot"',
+        imageURL:"https://goo.gl/images/RxJCkw",
+        correctAnswer:"<h1>I Robot</h1>"
+    },
+    {
+        difficulty:"easy",
+        instructions:'Make a div and inside a paragraph with "I Dragon"',
+        imageURL:"https://goo.gl/images/RxJCkw ",
+        correctAnswer:"<div><p>I Dragon</p></div>"
+    }
+    ];
 });
